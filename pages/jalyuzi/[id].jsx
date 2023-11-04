099bbcf349f99ba772f026f57d3c1c27702e9ce1
Farangiz17 <farangizismatova24@gmail.com>
2023-11-04 15:34:26 +0500

    fixed small bug

--- pages/jalyuzi/[id].jsx
+++ pages/jalyuzi/[id].jsx
@@ -4,13 +4,76 @@ import { useRouter } from 'next/router';
 import ProductItems from '~/components/partials/product/ProductItems';
 import PageContainer from '~/components/layouts/PageContainer';
 import FooterFullwidth from '~/components/shared/footers/FooterFullwidth';
-import dataJalyuzi from '~/utilities/dataJalyuzi';
+// import dataJalyuzi from '~/utilities/dataJalyuzi';
 
 const ProductCategoryScreen = () => {
     const Router = useRouter();
     const { id } = Router.query;
     const [category, setCategory] = useState(null);
     const [loading, setLoading] = useState(false);
+
+    const dataJalyuzi = [
+        {
+            id: 1,
+            image: '/static/jalyuzi/IMG_0401.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 2,
+            image: '/static/jalyuzi/IMG_0404.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 3,
+            image: '/static/jalyuzi/IMG_0422.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 4,
+            image: '/static/jalyuzi/IMG_0423.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 5,
+            image: '/static/jalyuzi/IMG_0428.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 6,
+            image: '/static/jalyuzi/IMG_0446.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 7,
+            image: '/static/jalyuzi/IMG_0451.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 8,
+            image: '/static/jalyuzi/IMG_0455.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 9,
+            image: '/static/jalyuzi/IMG_0466.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 10,
+            image: '/static/jalyuzi/IMG_0468.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        }
+    ];
 
     
     const item = dataJalyuzi.filter((item) => item.id == Number(id))
@@ -52,7 +115,7 @@ console.log('slug', id);
                 <div className="container">
                   <h2 className='text-gray py-3 text-center'> Jalyuzi - {id} </h2>
                    <div style={{display:'flex', justifyContent:'center'}}>
-                    <img src={`${item[0].image}`} alt="jalyuzi" />
+                    <img src={`${item[0]?.image}`} alt="jalyuzi" />
                    {/* <div 
                     style={{
                         backgroundImage:`url(${item[0].image})`,
--- pages/parda/[id].jsx
+++ pages/parda/[id].jsx
@@ -3,7 +3,7 @@ import BreadCrumb from '~/components/elements/BreadCrumb';
 import { useRouter } from 'next/router';
 import ProductItems from '~/components/partials/product/ProductItems';
 import PageContainer from '~/components/layouts/PageContainer';
-import data from '~/utilities/data';
+// import data from '~/utilities/data';
 import FooterFullwidth from '~/components/shared/footers/FooterFullwidth';
 
 const ProductCategoryScreen = () => {
@@ -11,6 +11,130 @@ const ProductCategoryScreen = () => {
     const { id } = Router.query;
     const [category, setCategory] = useState(null);
     const [loading, setLoading] = useState(false);
+
+    const data = [
+        {
+            id: 1,
+            image: '/static/pardalar/IMG_3523.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 2,
+            image: '/static/pardalar/IMG_3639.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 3,
+            image: '/static/pardalar/IMG_3640.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 4,
+            image: '/static/pardalar/IMG_3641.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 5,
+            image: '/static/pardalar/IMG_3642.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 6,
+            image: '/static/pardalar/IMG_3643.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 7,
+            image: '/static/pardalar/IMG_3645.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 8,
+            image: '/static/pardalar/IMG_3646.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 9,
+            image: '/static/pardalar/IMG_3647.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 10,
+            image: '/static/pardalar/IMG_3648.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 11,
+            image: '/static/pardalar/IMG_3649.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 12,
+            image: '/static/pardalar/IMG_3651.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 13,
+            image: '/static/pardalar/IMG_3652.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 14,
+            image: '/static/pardalar/IMG_3653.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 15,
+            image: '/static/pardalar/IMG_3655.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 16,
+            image: '/static/pardalar/IMG_3656.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 17,
+            image: '/static/pardalar/IMG_3657.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 18,
+            image: '/static/pardalar/IMG_3658.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 19,
+            image: '/static/pardalar/IMG_3659.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+        {
+            id: 20,
+            image: '/static/pardalar/IMG_3659.JPG',
+            price: 1000000,
+            description: 'baxmal',
+        },
+    ];
+    
 
     
     const item = data.filter((item) => item.id == Number(id))
@@ -53,7 +177,7 @@ console.log('slug', id);
                 <div className="container">
                   <h2 className='text-gray py-3 text-center'>Parda - {id} </h2>
                    <div style={{display:'flex', justifyContent:'center'}}>
-                    <img src={`${item[0].image}`} alt={`${item[0].image}`} />
+                    <img src={`${item[0]?.image}`} alt={`${item[0]?.image}`} />
                    {/* <div 
                     style={{
                         backgroundImage:`url(${item[0].image})`,
